58c0f53636e02ee49770ed82d9095a2d2f0f1b39
Pedro <pedroabernardes11@gmail.com>
2023-06-27 12:22:23 -0300

    feat: create button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { Button } from "./components/Button";
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    //<h1>Hello World</h1>
+    <div>
+    <Button text="Testando"/>
+    <Button />
+
+    </div>
   );
 }
 
